570f52e8f73f5f9dc5113c997ad1c97eef0afc09
David <sanderdj90@gmail.com>
2022-03-16 00:20:01 -0600

    fix?

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,8 @@
-import faker from "@faker-js/faker";
 
 const generateUsers = () =>
   [...Array(50)].map(() => {
-    const lastName = faker.name.lastName();
-    const bird = faker.animal.bird()
+    const lastName = 'Murray'
+    const bird = 'Blue Jay'
     return {
       avatar: `https://avatars.dicebear.com/api/human/${lastName}.svg`,
       lastName,
