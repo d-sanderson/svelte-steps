7dbc776ee55898e3aca75ec53a09125361109ac1
David <sanderdj90@gmail.com>
2022-03-16 00:34:56 -0600

    agin

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,16 @@
 <script>
-import ProductCard from "../components/ProductCard.svelte";
-
-
+	import ProductCard from '../components/ProductCard.svelte';
 	export let pokemons;
-    console.log(pokemons)
 </script>
 
 <div>
 	<main>
 		{#each pokemons as pokemon}
-			<ProductCard title={pokemon.name} author={pokemon.url} avatar="http://placekitten.com/200/300" />
+			<ProductCard
+				title={pokemon.name}
+				author={pokemon.url}
+				avatar="http://placekitten.com/200/300"
+			/>
 		{/each}
 	</main>
 </div>
